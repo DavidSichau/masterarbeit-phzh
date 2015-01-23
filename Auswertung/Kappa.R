ac0980141c013a4766c57d580b18145d8d877d75
David Sichau <git@sichau.eu>
2015-01-23 14:50:28 +0100

    cleanup

--- Auswertung/Kappa.R
+++ Auswertung/Kappa.R
@@ -1,6 +1,4 @@
-library(irr)
 library(psych)
-library(gmodels)
 
 david <- read.csv(file="./CodierungNeu3.csv", head=TRUE, sep=",")
 pitt <- read.csv(file="./Codierung_Pitt.csv", head=TRUE, sep=",")
@@ -9,56 +7,91 @@ david <- subset(david, Code%in%pitt$Code)
 pitt <- pitt[order(pitt$Code) , ]
 david <- david[order(david$Code) , ]
 
+#Test 201
+
 table(factor(david$M201_1.1, levels=0:1), factor(pitt$M201_1.1,levels=0:1))
 cohen.kappa(table(factor(david$M201_1.1, levels=0:1), factor(pitt$M201_1.1,levels=0:1)))
-table(factor(david$M201_1.1, levels=0:1), factor(pitt$M201_1.1,levels=0:1))
+table(factor(david$M201_1.2, levels=0:1), factor(pitt$M201_1.2,levels=0:1))
 cohen.kappa(table(factor(david$M201_1.2, levels=0:1), factor(pitt$M201_1.2,levels=0:1)))
-table(factor(david$M201_1.1, levels=0:1), factor(pitt$M201_1.1,levels=0:1))
-cohen.kappa(table(factor(david$M201_2.1, levels=0:2), factor(pitt$M201_2.1,levels=0:2)))
-table(factor(david$M201_1.1, levels=0:1), factor(pitt$M201_1.1,levels=0:1))
+
+table(factor(david$M201_2.1, levels=0:3), factor(pitt$M201_2.1,levels=0:3))
+cohen.kappa(table(factor(david$M201_2.1, levels=0:3), factor(pitt$M201_2.1,levels=0:3)))
+
+table(factor(david$M201_3.1, levels=0:2), factor(pitt$M201_3.1,levels=0:2))
 cohen.kappa(table(factor(david$M201_3.1, levels=0:2), factor(pitt$M201_3.1,levels=0:2)))
-table(factor(david$M201_1.1, levels=0:1), factor(pitt$M201_1.1,levels=0:1))
+table(factor(david$M201_3.2, levels=0:1), factor(pitt$M201_3.2,levels=0:1))
 cohen.kappa(table(factor(david$M201_3.2, levels=0:1), factor(pitt$M201_3.2,levels=0:1)))
-table(factor(david$M201_1.1, levels=0:1), factor(pitt$M201_1.1,levels=0:1))
+
+table(factor(david$M201_4.1, levels=0:1), factor(pitt$M201_4.1,levels=0:1))
 cohen.kappa(table(factor(david$M201_4.1, levels=0:1), factor(pitt$M201_4.1,levels=0:1)))
-table(factor(david$M201_1.1, levels=0:1), factor(pitt$M201_1.1,levels=0:1))
+table(factor(david$M201_4.2, levels=0:1), factor(pitt$M201_4.2,levels=0:1))
 cohen.kappa(table(factor(david$M201_4.2, levels=0:1), factor(pitt$M201_4.2,levels=0:1)))
-table(factor(david$M201_1.1, levels=0:1), factor(pitt$M201_1.1,levels=0:1))
+table(factor(david$M201_4.3, levels=0:1), factor(pitt$M201_4.3,levels=0:1))
 cohen.kappa(table(factor(david$M201_4.3, levels=0:1), factor(pitt$M201_4.3,levels=0:1)))
-table(factor(david$M201_1.1, levels=0:1), factor(pitt$M201_1.1,levels=0:1))
+table(factor(david$M201_4.4, levels=0:1), factor(pitt$M201_4.4,levels=0:1))
 cohen.kappa(table(factor(david$M201_4.4, levels=0:1), factor(pitt$M201_4.4,levels=0:1)))
-table(factor(david$M201_1.1, levels=0:1), factor(pitt$M201_1.1,levels=0:1))
+
+table(factor(david$M201_5.1, levels=0:1), factor(pitt$M201_5.1,levels=0:1))
 cohen.kappa(table(factor(david$M201_5.1, levels=0:1), factor(pitt$M201_5.1,levels=0:1)))
-table(factor(david$M201_1.1, levels=0:1), factor(pitt$M201_1.1,levels=0:1))
+table(factor(david$M201_5.2, levels=0:1), factor(pitt$M201_5.2,levels=0:1))
 cohen.kappa(table(factor(david$M201_5.2, levels=0:1), factor(pitt$M201_5.2,levels=0:1)))
-table(factor(david$M201_1.1, levels=0:1), factor(pitt$M201_1.1,levels=0:1))
 
+#Test 301
 
-
+table(factor(david$M301_1.1, levels=0:1), factor(pitt$M301_1.1,levels=0:1))
 cohen.kappa(table(factor(david$M301_1.1, levels=0:1), factor(pitt$M301_1.1,levels=0:1)))
+table(factor(david$M301_1.2, levels=0:1), factor(pitt$M301_1.2,levels=0:1))
 cohen.kappa(table(factor(david$M301_1.2, levels=0:1), factor(pitt$M301_1.2,levels=0:1)))
-cohen.kappa(table(factor(david$M301_2.1, levels=0:2), factor(pitt$M301_2.1,levels=0:2)))
+
+table(factor(david$M301_2.1, levels=0:3), factor(pitt$M301_2.1,levels=0:3))
+cohen.kappa(table(factor(david$M301_2.1, levels=0:3), factor(pitt$M301_2.1,levels=0:3)))
+
+table(factor(david$M301_3.1, levels=0:2), factor(pitt$M301_3.1,levels=0:2))
 cohen.kappa(table(factor(david$M301_3.1, levels=0:2), factor(pitt$M301_3.1,levels=0:2)))
+table(factor(david$M301_3.2, levels=0:1), factor(pitt$M301_3.2,levels=0:1))
 cohen.kappa(table(factor(david$M301_3.2, levels=0:1), factor(pitt$M301_3.2,levels=0:1)))
+
+table(factor(david$M301_4.1, levels=0:1), factor(pitt$M301_4.1,levels=0:1))
 cohen.kappa(table(factor(david$M301_4.1, levels=0:1), factor(pitt$M301_4.1,levels=0:1)))
+table(factor(david$M301_4.2, levels=0:1), factor(pitt$M301_4.2,levels=0:1))
 cohen.kappa(table(factor(david$M301_4.2, levels=0:1), factor(pitt$M301_4.2,levels=0:1)))
+table(factor(david$M301_4.3, levels=0:1), factor(pitt$M301_4.3,levels=0:1))
 cohen.kappa(table(factor(david$M301_4.3, levels=0:1), factor(pitt$M301_4.3,levels=0:1)))
+table(factor(david$M301_4.4, levels=0:1), factor(pitt$M301_4.4,levels=0:1))
 cohen.kappa(table(factor(david$M301_4.4, levels=0:1), factor(pitt$M301_4.4,levels=0:1)))
+
+table(factor(david$M301_5.1, levels=0:1), factor(pitt$M301_5.1,levels=0:1))
 cohen.kappa(table(factor(david$M301_5.1, levels=0:1), factor(pitt$M301_5.1,levels=0:1)))
+table(factor(david$M301_5.2, levels=0:1), factor(pitt$M301_5.2,levels=0:1))
 cohen.kappa(table(factor(david$M301_5.2, levels=0:1), factor(pitt$M301_5.2,levels=0:1)))
 
+#Test305
 
+table(factor(david$M305_1.1, levels=0:1), factor(pitt$M305_1.1,levels=0:1))
 cohen.kappa(table(factor(david$M305_1.1, levels=0:1), factor(pitt$M305_1.1,levels=0:1)))
+table(factor(david$M305_1.2, levels=0:1), factor(pitt$M305_1.2,levels=0:1))
 cohen.kappa(table(factor(david$M305_1.2, levels=0:1), factor(pitt$M305_1.2,levels=0:1)))
-cohen.kappa(table(factor(david$M305_2.1, levels=0:2), factor(pitt$M305_2.1,levels=0:2)))
-cohen.kappa(table(factor(david$M305_3.1, levels=0:2), factor(pitt$M305_3.1,levels=0:2)))
-cohen.kappa(table(factor(david$M305_3.2, levels=0:1), factor(pitt$M305_3.2,levels=0:1)))
-cohen.kappa(table(factor(david$M305_4.1, levels=0:1), factor(pitt$M305_4.1,levels=0:1)))
-cohen.kappa(table(factor(david$M305_4.2, levels=0:1), factor(pitt$M305_4.2,levels=0:1)))
-cohen.kappa(table(factor(david$M305_4.3, levels=0:1), factor(pitt$M305_4.3,levels=0:1)))
-cohen.kappa(table(factor(david$M305_4.4, levels=0:1), factor(pitt$M305_4.4,levels=0:1)))
-cohen.kappa(table(factor(david$M305_5.1, levels=0:1), factor(pitt$M305_5.1,levels=0:1)))
-cohen.kappa(table(factor(david$M305_5.2, levels=0:1), factor(pitt$M305_5.2,levels=0:1)))
 
+table(factor(david$M305_2.1, levels=0:2), factor(pitt$M305_2.1,levels=0:2))
+cohen.kappa(table(factor(david$M305_2.1, levels=0:2), factor(pitt$M305_2.1,levels=0:2)))
+
+table(factor(david$M305_3.1, levels=0:2), factor(pitt$M305_3.1,levels=0:2))
+cohen.kappa(table(factor(david$M305_3.1, levels=0:2), factor(pitt$M305_3.1,levels=0:2)))
+table(factor(david$M305_3.2, levels=0:1), factor(pitt$M305_3.2,levels=0:1))
+cohen.kappa(table(factor(david$M305_3.2, levels=0:1), factor(pitt$M305_3.2,levels=0:1)))
+
+table(factor(david$M305_4.1, levels=0:1), factor(pitt$M305_4.1,levels=0:1))
+cohen.kappa(table(factor(david$M305_4.1, levels=0:1), factor(pitt$M305_4.1,levels=0:1)))
+table(factor(david$M305_4.2, levels=0:1), factor(pitt$M305_4.2,levels=0:1))
+cohen.kappa(table(factor(david$M305_4.2, levels=0:1), factor(pitt$M305_4.2,levels=0:1)))
+table(factor(david$M305_4.3, levels=0:1), factor(pitt$M305_4.3,levels=0:1))
+cohen.kappa(table(factor(david$M305_4.3, levels=0:1), factor(pitt$M305_4.3,levels=0:1)))
+table(factor(david$M305_4.4, levels=0:1), factor(pitt$M305_4.4,levels=0:1))
+cohen.kappa(table(factor(david$M305_4.4, levels=0:1), factor(pitt$M305_4.4,levels=0:1)))
+
+table(factor(david$M305_5.1, levels=0:1), factor(pitt$M305_5.1,levels=0:1))
+cohen.kappa(table(factor(david$M305_5.1, levels=0:1), factor(pitt$M305_5.1,levels=0:1)))
+table(factor(david$M305_5.2, levels=0:1), factor(pitt$M305_5.2,levels=0:1))
+cohen.kappa(table(factor(david$M305_5.2, levels=0:1), factor(pitt$M305_5.2,levels=0:1)))
 
 
