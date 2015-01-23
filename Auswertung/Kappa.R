52b4bb67d4492cb12306c32a7f99ec6be60d8a3e
David Sichau <git@sichau.eu>
2015-01-23 14:45:48 +0100

    cohens kappa final

--- Auswertung/Kappa.R
+++ Auswertung/Kappa.R
@@ -9,18 +9,29 @@ david <- subset(david, Code%in%pitt$Code)
 pitt <- pitt[order(pitt$Code) , ]
 david <- david[order(david$Code) , ]
 
-
+table(factor(david$M201_1.1, levels=0:1), factor(pitt$M201_1.1,levels=0:1))
 cohen.kappa(table(factor(david$M201_1.1, levels=0:1), factor(pitt$M201_1.1,levels=0:1)))
+table(factor(david$M201_1.1, levels=0:1), factor(pitt$M201_1.1,levels=0:1))
 cohen.kappa(table(factor(david$M201_1.2, levels=0:1), factor(pitt$M201_1.2,levels=0:1)))
+table(factor(david$M201_1.1, levels=0:1), factor(pitt$M201_1.1,levels=0:1))
 cohen.kappa(table(factor(david$M201_2.1, levels=0:2), factor(pitt$M201_2.1,levels=0:2)))
+table(factor(david$M201_1.1, levels=0:1), factor(pitt$M201_1.1,levels=0:1))
 cohen.kappa(table(factor(david$M201_3.1, levels=0:2), factor(pitt$M201_3.1,levels=0:2)))
+table(factor(david$M201_1.1, levels=0:1), factor(pitt$M201_1.1,levels=0:1))
 cohen.kappa(table(factor(david$M201_3.2, levels=0:1), factor(pitt$M201_3.2,levels=0:1)))
+table(factor(david$M201_1.1, levels=0:1), factor(pitt$M201_1.1,levels=0:1))
 cohen.kappa(table(factor(david$M201_4.1, levels=0:1), factor(pitt$M201_4.1,levels=0:1)))
+table(factor(david$M201_1.1, levels=0:1), factor(pitt$M201_1.1,levels=0:1))
 cohen.kappa(table(factor(david$M201_4.2, levels=0:1), factor(pitt$M201_4.2,levels=0:1)))
+table(factor(david$M201_1.1, levels=0:1), factor(pitt$M201_1.1,levels=0:1))
 cohen.kappa(table(factor(david$M201_4.3, levels=0:1), factor(pitt$M201_4.3,levels=0:1)))
+table(factor(david$M201_1.1, levels=0:1), factor(pitt$M201_1.1,levels=0:1))
 cohen.kappa(table(factor(david$M201_4.4, levels=0:1), factor(pitt$M201_4.4,levels=0:1)))
+table(factor(david$M201_1.1, levels=0:1), factor(pitt$M201_1.1,levels=0:1))
 cohen.kappa(table(factor(david$M201_5.1, levels=0:1), factor(pitt$M201_5.1,levels=0:1)))
+table(factor(david$M201_1.1, levels=0:1), factor(pitt$M201_1.1,levels=0:1))
 cohen.kappa(table(factor(david$M201_5.2, levels=0:1), factor(pitt$M201_5.2,levels=0:1)))
+table(factor(david$M201_1.1, levels=0:1), factor(pitt$M201_1.1,levels=0:1))
 
 
 
--- Auswertung/Rasch.R
+++ Auswertung/Rasch.R
@@ -14,10 +14,11 @@ plotjointICC(result,  item.subset = c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15),
              )
 
 
-
-
-
+plotINFO(result)
+plotPImap(result)
+plotPWmap(result)
 plotICC(result)
+
 
 
 pp <- person.parameter(result)
