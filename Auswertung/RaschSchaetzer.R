fe0a9f7603f3a431f2356f5df11c71b19c9749f6
David Sichau <git@sichau.eu>
2015-01-28 14:55:56 +0100

    added parameter estimation comparision

---
+++ Auswertung/RaschSchaetzer.R
@@ -0,0 +1,18 @@
+library(eRm)
+library(ltm)
+library(ggplot2)
+data <- read.csv(file="./CodierungNeu3.csv", head=TRUE, sep=",")
+raschData <- subset(data, select = c(M201_Q1  , M201_Q2  , M201_Q3  , M201_Q4  , M201_Q5,M301_Q1  , M301_Q2  , M301_Q3  , M301_Q4  , M301_Q5,M305_Q1  , M305_Q2  , M305_Q3  , M305_Q4  , M305_Q5   ))
+
+rm1 <- RM(raschData)
+rm2 <- rasch(raschData, start.val="random", constraint = cbind(ncol(raschData)+1,1))
+
+df<-data.frame(x=rm2$coefficients[,1],y=rm1$betapar)
+theme_set(theme_grey(base_size = 18)) 
+ggplot(df,aes(x=rm2$coefficients[,1], y= rm1$betapar)) +
+  geom_point(shape=1,size=5) +
+  geom_smooth(method=lm) + # Add linear regression line 
+  xlab("marginaler Maximum-Likelihood") +
+  ylab("bedingter Maximum-Likelihood") +
+  ggtitle("Vergleich Parameter Schätzer")
+ggsave(file="RaschVergleich.png")
